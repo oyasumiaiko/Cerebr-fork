1c68ca2d35296eff33c8f5f8a394170839c761ad
n2yt584v2t4nh7y <117180266+n2yt584v2t4nh7y@users.noreply.github.com>
2025-08-13 02:32:03 +0800

    添加纯对话模式PLACEHOLDER

--- src/core/message_sender.js
+++ src/core/message_sender.js
@@ -129,6 +129,7 @@ export function createMessageSender(appContext) {
     isTemporaryMode = true;
     GetInputContainer().classList.add('temporary-mode');
     document.body.classList.add('temporary-mode');
+    messageInput.setAttribute('placeholder', '纯对话模式，输入消息...');
     try {
       document.dispatchEvent(new CustomEvent('TEMP_MODE_CHANGED', { detail: { isOn: true } }));
     } catch (_) {}
@@ -142,6 +143,7 @@ export function createMessageSender(appContext) {
     isTemporaryMode = false;
     GetInputContainer().classList.remove('temporary-mode');
     document.body.classList.remove('temporary-mode');
+    messageInput.setAttribute('placeholder', '输入消息...');
     try {
       document.dispatchEvent(new CustomEvent('TEMP_MODE_CHANGED', { detail: { isOn: false } }));
     } catch (_) {}
